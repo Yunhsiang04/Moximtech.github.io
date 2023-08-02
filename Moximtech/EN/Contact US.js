c33fde480b6dc07a76023dc10cb3e86850c252f7
yunhsiang <Vito.lin2004@gmail.com>
2023-08-02 22:51:14 +0800

    Moximtech

---
+++ Moximtech/EN/Contact US.js
@@ -0,0 +1,17 @@
+var companyname = document.getElementById("companyname");
+var username = document.getElementById("username");
+var emailText = document.getElementById("emailText");
+var subject = document.getElementById("subject");
+var body = document.getElementById("body");
+var btn = document.getElementById("btn");
+var to = "vickychan@moximtech.com";
+
+btn.addEventListener("click", function(){
+    var message = body.value +'%0A%0A';
+        message += "Company:"+ companyname.value + '%0A';
+        message += "Form:" + username.value + '%0A';
+        message += "Email:" + emailText.value;
+
+    mailTo.href = "mailto:" + to + "?subject=" + subject.value + "&body=" + message;
+    mailTo.click();
+})
